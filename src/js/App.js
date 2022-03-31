893f580f2139289acd2aea961dddc549dad695eb
kassu11 <61124141+kassu11@users.noreply.github.com>
2022-03-31 21:16:45 +0300

    useState test
    - Testasin miten useState toimii ja myös että toimiiko youtube iframe

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,25 @@
 import "../scss/App.scss";
+import {useState} from "react";
 
-function App() {
+function random(a = 1, b = 0) {
+  return Math.round(Math.random() * (a - b)) + b;
+}
+
+function App(obj) {
+  const [counter, setCounter] = useState(50);
+  const [color, setColor] = useState(`rgb(${random(50, 255)}, ${random(50, 255)}, ${random(50, 255)})`);
   return (
+    <>
     <div className="App">
-      <h1>Title</h1>
-      <button>Nappi</button>
+      <h1 style={{background: color}}>{`Tulos: ${counter}`}</h1>
+      <button onClick={v => {
+        setCounter(counter + 1)
+        setColor(`rgb(${random(50, 255)}, ${random(50, 255)}, ${random(50, 255)})`)
+      }}>Nosta tulosta</button>
     </div>
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/Tn6-PIqc4UM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </>
   );
 }
 
